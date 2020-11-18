89410c40b60c76b3193505c1edcaefc33d6c73e2
Gustavo Coronel <gcoronelc@gmail.com>
2020-11-17 22:02:13 -0500

    Clase 01

---
+++ UNI02/Demo02/src/main/java/pe/uni/demo02/Programa01.java
@@ -0,0 +1,16 @@
+package pe.uni.demo02;
+
+/**
+ * @author Eric Gustavo Coronel Castillo
+ * @blog www.desarrollasoftware.com
+ * @email gcoronelc@gmail.com
+ * @youtube www.youtube.com/c/DesarrollaSoftware
+ * @facebook www.facebook.com/groups/desarrollasoftware/
+ */
+public class Programa01 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+	}
+
+}
